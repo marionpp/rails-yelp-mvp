1664e1186485a416c4c533a63d24e48168a94901
marion petitprez <marion.petitprez@gmail.com>
2021-02-24 14:55:02 +0100

    Controller

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,8 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+	CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :category, inclusion: { in: CATEGORIES }
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ApplicationRecord
+	belongs_to :restaurant
+  validates :rating, inclusion: { in: 0..5 }
+	validates :content, presence: true
+	validates :rating, numericality: { only_integer: true }
 end
